0a4586462bf0c6feb85b9bd45da0959b0278f783
Santosl2 <mfilype2019@gmail.com>
2025-07-25 22:55:14 -0300

    feat: comment out info logs for opening and removing old closed sessions

--- src/session_record.js
+++ src/session_record.js
@@ -278,7 +278,7 @@ class SessionRecord {
         if (!this.isClosed(session)) {
             console.warn("Session already open");
         }
-        console.info("Opening session:", session);
+        //console.info("Opening session:", session);
         session.indexInfo.closed = -1;
     }
 
@@ -298,7 +298,7 @@ class SessionRecord {
                 }
             }
             if (oldestKey) {
-                console.info("Removing old closed session:", oldestSession);
+               // console.info("Removing old closed session:", oldestSession);
                 delete this.sessions[oldestKey];
             } else {
                 throw new Error('Corrupt sessions object');
