5aeb0b55ca1ad19f6ecbdb5dee3194bafe79dc6d
Santosl2 <mfilype2019@gmail.com>
2025-10-31 15:38:17 -0300

    fix: improve error message in verifyMAC for better debugging

--- src/crypto.js
+++ src/crypto.js
@@ -81,10 +81,12 @@ function deriveSecrets(input, salt, info, chunks) {
 function verifyMAC(data, key, mac, length) {
     const calculatedMac = calculateMAC(key, data).subarray(0, length);
     if (mac.length !== length || calculatedMac.length !== length) {
-        throw new Error("Bad MAC length");
+        throw new Error("Bad MAC length Expected: " + length +
+            " Got: " + mac.length + " and " + calculatedMac.length);
     }
     if (!mac.equals(calculatedMac)) {
-        throw new Error("Bad MAC");
+       throw new Error("Bad MAC Expected: " + calculatedMac.toString('hex') +
+            " Got: " + mac.toString('hex'));
     }
 }
 
